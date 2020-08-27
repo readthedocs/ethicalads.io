4e285803fa5a00cf5c1d9641ef15c5e06593513a
Eric Holscher <eric@ericholscher.com>
2020-08-27 14:53:03 -0700

    Wwatch includdes

--- tasks.py
+++ tasks.py
@@ -99,7 +99,7 @@ def livereload(c):
     theme_path = SETTINGS["THEME"]
     server.watch("{}/templates/*.html".format(theme_path), lambda: build(c))
     server.watch("{}/templates/ea/*.html".format(theme_path), lambda: build(c))
-    server.watch("{}/templates/**.html".format(theme_path), lambda: build(c))
+    server.watch("{}/templates/ea/includes/*.html".format(theme_path), lambda: build(c))
     static_file_extensions = [".css", ".js"]
     for extension in static_file_extensions:
         static_file = "{0}/static/**/*{1}".format(theme_path, extension)
