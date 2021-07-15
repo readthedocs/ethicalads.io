f506f3b8875ef03181abc2cfb5b104acb761e7a0
David Fischer <david@readthedocs.org>
2021-07-15 12:03:33 -0700

    Marketing funnel tweaks
    - Store "?ref" and "?refferal"
    - Don't overwrite values - keep the original
    - Use prefixed form fields for these values

--- static-src/marketing.js
+++ static-src/marketing.js
@@ -4,30 +4,38 @@ import * as url from 'js-url';
 
 // This helps our marketing funnel.
 //
-// Specifically, this looks for the query parameter ("referral")
+// Specifically, this looks for the query parameters "referral" and "ref"
 // and sets a short duration cookie for that.
-// If an inbound form is submitted, this value will be appended
+// If an inbound form is submitted, these will be appended
 // to the form so we can see how a referral came to us.
 // This is used to count publisher and advertiser referrals
 $(function () {
   let cookieName = 'ethicalads-referrer';
   let referrer = $.url('?referral');
+  let ref = $.url('?ref');  // Used internally by RTD
   let expiry = 7;  // days
+  let cookieVal = Cookies.getJSON(cookieName) || {};
 
-  if (referrer) {
-    // Set the referrer which will submit if a form is submitted
-    Cookies.set(cookieName, referrer, { expires: expiry });
-  } else {
-    referrer = Cookies.get(cookieName);
+  if (referrer && !cookieVal["referrer"]) {
+    cookieVal["referrer"] = referrer;
+  }
+  if (ref && !cookieVal["ref"]) {
+    cookieVal["ref"] = ref;
+  }
+
+  if (Object.keys(cookieVal).length > 0 && cookieVal.constructor === Object) {
+    Cookies.set(cookieName, cookieVal, { expires: expiry });
   }
 
   // Append the referrer to the inbound form
   let form = document.forms['fa-form-1']
-  if (form !== undefined && referrer) {
-    let input = document.createElement("input");
-    input.setAttribute("type", "hidden");
-    input.setAttribute("name", "referrer");
-    input.setAttribute("value", referrer);
-    form.appendChild(input);
+  if (form !== undefined && cookieVal) {
+    for (const key in cookieVal) {
+      let input = document.createElement("input");
+      input.setAttribute("type", "hidden");
+      input.setAttribute("name", "_" + key);
+      input.setAttribute("value", cookieVal[key]);
+      form.appendChild(input);
+    }
   }
 });
