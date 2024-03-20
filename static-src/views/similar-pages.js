4fb7c666f5f10d3136358d4c2409a6131269ac55
David Fischer <david@readthedocs.org>
2024-03-20 16:48:46 -0700

    Use AJAX to query the ad server for similar pages

--- static-src/views/similar-pages.js
+++ static-src/views/similar-pages.js
@@ -7,6 +7,9 @@ function SimilarPagesViewModel() {
 
   const initialUrl = params.get('url') || "";
 
+  // const SIMILAR_PAGES_URL = "http://ethicaladserver.lan:5000/api/v1/similar-pages/";
+  const SIMILAR_PAGES_URL = "https://server.ethicalads.io/api/v1/similar-pages/";
+
   this.url = ko.observable(initialUrl);
   this.results = ko.observable([]);
   this.state = ko.observable();
@@ -43,23 +46,33 @@ function SimilarPagesViewModel() {
     this.state("loading");
     this.results([]);
 
-    // Replace with an AJAX call to the ad server
-    setTimeout(function (viewmodel) {
-      viewmodel.results([{
-          "url": "https://mongoengine-odm.readthedocs.io/",
-          "title": "MongoEngine User Documentation",
-          "image": "https://mongoengine-odm.readthedocs.io/favicon.ico",
-          "description": "MongoEngine is an Object-Document Mapper, written in Python for working with MongoDB. To install it, simply run python -m pip install -U mongoengine",
-        },
-        {
-          "url": "https://doc.ivre.rocks/en/latest/overview/principles.html",
-          "title": "Principles - IVRE Documentation",
-          "image": "https://doc.ivre.rocks/favicon.ico",
-          "description": "IVRE is a network cartography (or network recon) framework. IVRE has five purposes (we use this word to refer to the different types of data IVRE handles), which can be stored by one or more backend databases:",
-        },
-      ]);
+    let viewmodel = this;
+
+    $.ajax({
+      "method": "GET",
+      "url": SIMILAR_PAGES_URL,
+      "data": {"url": this.url()},
+    })
+    .done(function(data) {
+      let res = [];
+      data.results.forEach(function (result) {
+        let url = new URL(result.url);
+        let image = url.origin + "/favicon.ico";
+        res.push({
+          "title": result.title,
+          "description": result.description,
+          "url": result.url,
+          "distance": result.distance,
+          "image": image,
+        });
+      });
+      viewmodel.results(res);
       viewmodel.state("loaded");
-    }, 1000, this);
+    })
+    .fail(function(data) {
+      viewmodel.state("error");
+      console.debug("Error getting similar pages", data);
+    });
   };
 
   // Fire the form submission if there's a passed URL
